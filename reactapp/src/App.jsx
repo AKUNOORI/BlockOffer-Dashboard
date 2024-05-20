594ba4d95df47cd78c6129b7aa351b16f9690e26
Ajay <ajayakunoori1997@gmail.com>
2024-05-20 11:08:26 +0530

    first commit

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import VisualizationComponent from './VisualizationComponent.jsx'
-// import PostData from './postData.jsx'
-
-
-// function App() {
-
-//   return (
-//     <>
-//     <PostData/>
-//     <VisualizationComponent/>
-//     </>
-//   )
-// }
-
-// export default App
-
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -26,7 +8,6 @@ const App = () => {
         <Router>
             <Routes>
                 <Route path="/" element={<PostData />} />
-                {/* <Route path="/VisualizationComponent" element={<VisualizationComponent />} /> */}
             </Routes>
         </Router>
     );
